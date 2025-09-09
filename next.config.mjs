bc390f77be95ed3b5a1168a8bfa0389f20a4a94a
Digvijay Nath <itsdiju@gmail.com>
2025-09-10 00:42:35 +0530

    Prepare for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  output: "export", // Required for static export to GitHub Pages
+};
 
-export default nextConfig
+export default nextConfig;
